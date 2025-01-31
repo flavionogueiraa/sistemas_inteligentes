a58236364290a3d8abee76b940acb08339d3a926
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2025-01-31 16:27:34 -0300

    subida de encosta

---
+++ unidade2/subida_encosta/subida.py
@@ -0,0 +1,42 @@
+import math
+import random
+
+
+def f(x):
+    """Função objetivo."""
+    return (1 / (x * x + 1)) * math.cos(10 * x)
+
+
+class Estado:
+    def __init__(self, x):
+        self.x = x
+        self.f = f(x)
+
+
+def exibe(estado):
+    print(f"x = {estado.x}, f = {estado.f}")
+
+
+def estado_inicial():
+    return Estado(random.uniform(-10, 10))
+
+
+def melhor_sucessor(estado):
+    melhor = estado
+    atual = estado
+    for _ in range(5):
+        deltax = random.uniform(-0.01, 0.01)
+        vizinho = Estado(atual.x + deltax)
+        if vizinho.f > melhor.f:
+            melhor = vizinho
+
+    return melhor
+
+
+atual = estado_inicial()
+while True:
+    vizinho = melhor_sucessor(atual)
+    if vizinho.f <= atual.f:
+        exibe(atual)
+        break
+    atual = vizinho
